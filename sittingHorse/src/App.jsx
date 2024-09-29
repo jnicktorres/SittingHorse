bfb3799e371c318b80f9fe212daeec741e87f26f
Nick Torres <jnicholastorres1@gmail.com>
2024-09-29 01:25:54 -0700

    Add routing for Home Page and Character Sheet Page.
    Use this commit as a template for adding new pages to the routing in the future.

--- sittingHorse/src/App.jsx
+++ sittingHorse/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import CharacterSheetPage from "./pages/CharacterSheetPage.jsx";
 
 function App() {
   const [array, setArray] = useState([]);
@@ -16,16 +19,10 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div>
-        {array.map((fruit, index) => (
-          <div key={index}>
-            <p>{fruit}</p>
-            <br></br>
-          </div>
-        ))}
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/charactersheet" element={<CharacterSheetPage />} />
+    </Routes>
   );
 }
 
---
+++ sittingHorse/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <header className="HomePageHeader">
+        <h1>Home Page</h1>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
